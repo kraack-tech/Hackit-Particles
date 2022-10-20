73c8e98091d71d3ade68675b50f449d1e6c44258
kraack-tech <kriskraack@gmail.com>
2022-10-20 12:07:55 +0200

    Add challenges

--- sketch.js
+++ sketch.js
@@ -4,34 +4,55 @@
 // Adapted from https://github.com/nature-of-code/
 // released under MIT license
 
-var ball;
+//global balls variable
+var balls;
+balls = [];
+value = 0;
+
 ////////////////////////////////////////////////////
 function setup() {
   createCanvas(900, 600);
-  ball = new Ball();
+  //ball = new Ball();
+
+
+  for (var i = 0; i < balls.length; i++) {
+    balls.push(new Ball());
+  }
+  console.log(balls)
+
+  
+
 }
 ////////////////////////////////////////////////////
 function draw() {
-  background(0);
+  background(value);
+  // for loop that draws each ball in the 'balls' array
+  for (var i = 0; i < balls.length; i++) {
+    var gravity = createVector(0, 0.1);
+    var friction = balls[i].velocity.copy();
+    friction.mult(-1);
+    friction.normalize();
+    friction.mult(0.01);
+    balls[i].applyForce(friction);
+    balls[i].applyForce(gravity);
 
-  var gravity = createVector(0, 0.1);
-  var friction = ball.velocity.copy();
-  friction.mult(-1);
-  friction.normalize();
-  friction.mult(0.01);
-  ball.applyForce(friction);
-  ball.applyForce(gravity);
+    balls[i].run()
+    //console.log(balls[i])
+  }
 
-  ball.run();
+  //add button option title
+  addTitle();
+
+  //ball.run();
 }
 //////////////////////////////////////////////////////
 class Ball {
 
-  constructor(){
-    this.velocity = new createVector(5, 0);
-    this.location = new createVector(width/2, 50);
+  constructor(x, y){
+    this.velocity = new createVector(random(-3,3), random(-3,3));
+    this.location = new createVector(x, y);
     this.acceleration = new createVector(0, 0);
-    this.size = 40;
+    this.size = random(10, 40);
   }
 
   run(){
@@ -41,7 +62,10 @@ class Ball {
   }
 
   draw(){
-    fill(125);
+    var ballColor = color(0, 153, 255);
+    ballColor.setAlpha(90 + 90 * sin(1000));
+    fill(ballColor);
+    noStroke();
     ellipse(this.location.x, this.location.y, this.size, this.size);
   }
 
@@ -69,3 +93,36 @@ class Ball {
     this.acceleration.add(force);
   }
 }
+
+function mouseDragged() {
+  balls.push(new Ball(mouseX, mouseY));
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    balls = [];
+  } else if (keyCode === RIGHT_ARROW) {
+    if(value === 0){
+      value = 255;
+    } else if (value = 255){
+      value = 0;
+    }
+  }
+}
+
+
+  // Function that draws the title and subtitle
+  function addTitle() {
+    this.title = 'Hackit: Particles'
+    this.subTitle = 'Press mouse to add perticles'
+    this.subTitle2 = 'Press "<-" to clear particles or "->" to change background color'
+    fill(0, 172, 230);
+    noStroke();
+    textAlign('center', 'center');
+    textSize(16);
+    text(this.title,width/2,height/20);
+    textSize(12);
+    text(this.subTitle,width/2,height/13);
+    text(this.subTitle2,width/2,height/9);
+    
+  };
